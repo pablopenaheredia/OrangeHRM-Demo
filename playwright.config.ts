0a07e8325b7791e1ac102285604db6495a52bd17
Pablo Pena Heredia <pablopenaherediagmail.com>
2025-03-07 00:08:20 -0300

    update Playwright configuration and test report paths

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  outputDir: '/test-results/',
+  outputDir: 'test-results',
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: false,
