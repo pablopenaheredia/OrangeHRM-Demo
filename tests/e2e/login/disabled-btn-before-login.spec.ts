30fd274e6580e008b1616c3c13f451f89936dc7e
Pablo Pena Heredia <pablopenaherediagmail.com>
2025-03-09 00:10:07 -0300

    Update tests login

--- tests/e2e/login/disabled-btn-before-login.spec.ts
+++ tests/e2e/login/disabled-btn-before-login.spec.ts
@@ -1,6 +1,5 @@
 import { expect, test } from '../../fixtures';
 
-
 test.describe('[US01] Inicio de sesión | Boton Deshabilitado sin credenciales', () => {
     
     test('Validar boton login deshabilitado cuando no hay credenciales ingresadas', async ({ loginPage }) => {
