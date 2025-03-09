6c1626e9fac5c03d4cd9be29b07d284ead121803
Pablo Agustin Pena Heredia <107514101+pablopenaheredia@users.noreply.github.com>
2025-03-09 06:05:35 -0300

    Update add-employee.spec.ts

--- tests/e2e/employees/add-employee.spec.ts
+++ tests/e2e/employees/add-employee.spec.ts
@@ -2,13 +2,13 @@ import { expect, test } from '../../fixtures';
 
 
 test.describe('[US02] Gestión de empleados | Agregar nuevos empleados', () => {
-
+const randomID = Math.floor(Math.random()*(100000-1+1)+1).toString();
     test('Validar agregar un empleado exitosamente', async ({ loginPage, employeePage }) => {
         await loginPage.goToLoginPage();
         await loginPage.login();
         await employeePage.goToAddEmployeePage();
-        await employeePage.fillAddEmployee("Juan", 'Perez', Math.floor(Math.random()*(100000-1+1)+1).toString());
+        await employeePage.fillAddEmployee("Juan", 'Perez', randomID);
         await employeePage.saveClick();
-        await employeePage.checkEmployeeIsAdded('Juan', 'Perez', '12345');
+        await employeePage.checkEmployeeIsAdded('Juan', 'Perez', randomID);
     });
 });
