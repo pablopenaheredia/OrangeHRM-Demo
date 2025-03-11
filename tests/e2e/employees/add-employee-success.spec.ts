3bd25b1bdc6737fa4fd72d3e8a276fd02ff489c6
Pablo Agustin Pena Heredia <107514101+pablopenaheredia@users.noreply.github.com>
2025-03-11 17:37:41 -0300

    Update add-employee-success.spec.ts

--- tests/e2e/employees/add-employee-success.spec.ts
+++ tests/e2e/employees/add-employee-success.spec.ts
@@ -1,19 +1,36 @@
 import { expect, test } from '../../fixtures';
 
+test.beforeEach(async ({ loginPage }) => {
+    await loginPage.goToLoginPage();
+    await loginPage.login();
+});
 
 test.describe('[US02] Gestión de empleados | Agregar nuevos empleados', () => {
 
-    test('Validar agregar un empleado exitosamente', async ({ loginPage, employeePage }) => {
+    test('Escenario 1 | Validar agregar un empleado exitosamente', async ({ employeePage }) => {
         
         const uniqueID = await employeePage.generateUniqueID();
-        await loginPage.goToLoginPage();
-        await loginPage.login();
+        await test.step('Dado que el usuario se encuenrta en la pagina de inicio de sesión', async () => { });
+        await test.step('Cuando el usuario navega a la página de empleados', async () => {
+            await employeePage.clickOnPIMModule();
+        });
+           await test.step('Y se dirige hacia la sección de agregar empleados', async () => {
         await employeePage.goToAddEmployeePage();
-        await employeePage.fillAddEmployee("messi", "dios", uniqueID);
-        await employeePage.saveClick();
-        await employeePage.page.waitForLoadState('domcontentloaded');
-        await employeePage.page.waitForTimeout(5000);
-        const isEmployeeAdded = await employeePage.checkEmployeeIsAdded("messi", 'dios', uniqueID);
-        expect(isEmployeeAdded).toBe(true);
+        });
+        await test.step('Y el usuario llena los campos requeridos', async () => {
+            await employeePage.fillAddEmployee("messi", "dios", uniqueID);
+        });
+        await test.step('Y el usuario guarda al empleado', async () => {
+            await employeePage.saveClick();
+        });
+
+        await test.step('Y el usuario busca al empleado por su ID', async () => {
+            await employeePage.clickOnPIMModule();
+            await employeePage.fillEmployeeInfoIDInput(uniqueID);
+            await employeePage.searchClick();
+        });
+        await test.step('Entonces el empleado se encuentra en la lista de empleados', async () => {
+            await expect.soft(employeePage.idColumnValues(uniqueID)).toBeVisible();
+        });
     });
 });
