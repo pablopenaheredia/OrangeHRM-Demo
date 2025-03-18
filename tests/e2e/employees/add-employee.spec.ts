9b55edbfd03ec06433f6433106e0686c0531d20d
Pablo Pena Heredia <pablopenaherediagmail.com>
2025-03-18 03:38:25 -0300

    new structure

--- tests/e2e/employees/add-employee.spec.ts
+++ tests/e2e/employees/add-employee.spec.ts
@@ -5,17 +5,55 @@ test.beforeEach(async ({ loginPage }) => {
     await loginPage.login();
 });
 
-test.describe('[US02] Gestión de empleados | Agregar empleados incorrectamente', () => {
+test.describe('[US02] Gestión de empleados | Agregar y validar empleados', () => {
 
-    test('Validar agregar un empleado sin nombre', async ({ employeePage }) => {
+    test('Escenario 1 | Validar agregar un empleado exitosamente', async ({ employeePage }) => {
+        const uniqueID = await employeePage.generateUniqueID();
+
+        await test.step('Dado que el usuario navega a la seccion de agregar empleados', async () => {
+            await employeePage.clickOnPIMModule();
+            await employeePage.goToAddEmployeePage();
+        });
+
+        await test.step('Y el usuario llena los campos y agrega al empleado', async () => {
+            await employeePage.fillAddEmployee({
+                firstName: "Sam",
+                lastName: "Tarly",
+                employeeID: uniqueID
+            });
+            const responsePromiseAddEmployee = employeePage.page.waitForResponse(response =>
+                response.url().includes('/api/v2/pim/employees')
+                && response.status() === 200
+                && response.request().method() === 'POST'
+            );
+            await employeePage.saveNewEmployeeClick();
+            await responsePromiseAddEmployee;
+        });
+
+        await test.step('Cuando el usuario busca al empleado por su ID', async () => {
+            await employeePage.clickOnPIMModule();
+            await employeePage.fillEmployeeInfoIDInput(uniqueID);
+            await employeePage.searchClick();
+        });
+
+        await test.step('Entonces el empleado se encuentra en la lista de empleados', async () => {
+            const locator = employeePage.idColumnValues(uniqueID);
+            await locator.waitFor({ state: 'visible', timeout: 10000 });
+        });
+    });
+
+    test('Escenario 2 | Validar agregar un empleado sin nombre', async ({ employeePage }) => {
         const uniqueID = await employeePage.generateUniqueID();
         await test.step('Dado que el usuario se encuentra en la página de inicio de sesión', async () => { });
+
         await test.step('Cuando el usuario navega a la página de empleados', async () => {
             await employeePage.clickOnPIMModule();
         });
+
         await test.step('Y se dirige hacia la sección de agregar empleados', async () => {
             await employeePage.goToAddEmployeePage();
         });
+
         await test.step('Y el usuario llena los campos requeridos sin nombre', async () => {
             await employeePage.fillAddEmployee({
                 firstName: "",
@@ -24,101 +62,125 @@ test.describe('[US02] Gestión de empleados | Agregar empleados incorrectamente'
             });
             await employeePage.saveNewEmployeeClick();
         });
+
         await test.step('Entonces se muestra un mensaje de error', async () => {
             await employeePage.page.waitForSelector("//div[@class='oxd-input-group oxd-input-field-bottom-space']//span[1]");
             await expect(employeePage.page.locator("//div[@class='oxd-input-group oxd-input-field-bottom-space']//span[1]")).toHaveText('Required');
         });
+
         await test.step('Entonces el empleado no se encuentra en la lista de empleados', async () => {
             await expect.soft(employeePage.idColumnValues(uniqueID)).toBeHidden();
         });
     });
 
-    test('Validar agregar un empleado sin apellido', async ({ employeePage }) => {
+    test('Escenario 3 | Validar agregar un empleado sin apellido', async ({ employeePage }) => {
         const uniqueID = await employeePage.generateUniqueID();
         await test.step('Dado que el usuario se encuentra en la página de inicio de sesión', async () => { });
+
         await test.step('Cuando el usuario navega a la página de empleados', async () => {
             await employeePage.clickOnPIMModule();
         });
+
         await test.step('Y se dirige hacia la sección de agregar empleados', async () => {
             await employeePage.goToAddEmployeePage();
         });
+
         await test.step('Y el usuario llena los campos requeridos sin apellido', async () => {
             await employeePage.fillAddEmployee("John", '', uniqueID);
         });
+
         await test.step('Y el usuario guarda al empleado', async () => {
             await employeePage.saveNewEmployeeClick();
         });
+
         await test.step('Entonces se muestra un mensaje de error', async () => {
             await employeePage.page.waitForSelector("//div[@class='oxd-input-group oxd-input-field-bottom-space']//span[1]");
             await expect(employeePage.page.locator("//div[@class='oxd-input-group oxd-input-field-bottom-space']//span[1]")).toHaveText('Required');
         });
+
         await test.step('Entonces el empleado no se encuentra en la lista de empleados', async () => {
             await expect.soft(employeePage.idColumnValues(uniqueID)).toBeHidden();
         });
     });
 
-    test('Validar agregar un empleado sin ID', async ({ employeePage }) => {
-        
+    test('Escenario 4 | Validar agregar un empleado sin ID', async ({ employeePage }) => {
         await test.step('Dado que el usuario se encuentra en la página de inicio de sesión', async () => { });
+
         await test.step('Cuando el usuario navega a la página de empleados', async () => {
             await employeePage.clickOnPIMModule();
         });
+
         await test.step('Y se dirige hacia la sección de agregar empleados', async () => {
             await employeePage.goToAddEmployeePage();
         });
+
         await test.step('Y el usuario llena los campos requeridos sin ID', async () => {
             await employeePage.fillAddEmployee("John", 'Doe', '');
         });
+
         await test.step('Y el usuario guarda al empleado', async () => {
             await employeePage.saveNewEmployeeClick();
         });
+
         await test.step('Entonces se muestra un mensaje de error', async () => {
             await employeePage.page.waitForSelector("//span[text()='Required']");
             await expect(employeePage.page.locator("//span[text()='Required']")).toHaveText('Required');
         });
+
         await test.step('Entonces el empleado no deberia aparecer en la lista de empleados', async () => {
             await expect.soft(employeePage.lastNameValues('Doe')).toBeVisible();
         });
     });
 
-    test('Validar agregar un empleado con ID duplicado', async ({ employeePage }) => {
+    test('Escenario 5 | Validar agregar un empleado con ID duplicado', async ({ employeePage }) => {
         const uniqueID = await employeePage.generateUniqueID();
         await test.step('Dado que el usuario se encuenrta en la pagina de inicio de sesión', async () => { });
+
         await test.step('Cuando el usuario navega a la página de empleados', async () => {
             await employeePage.clickOnPIMModule();
         });
-           await test.step('Y se dirige hacia la sección de agregar empleados', async () => {
-        await employeePage.goToAddEmployeePage();
+
+        await test.step('Y se dirige hacia la sección de agregar empleados', async () => {
+            await employeePage.goToAddEmployeePage();
         });
+
         await test.step('Y el usuario llena los campos requeridos', async () => {
             await employeePage.fillAddEmployee("Pa", "blo", uniqueID);
         });
+
         await test.step('Y el usuario guarda al empleado', async () => {
             await employeePage.saveNewEmployeeClick();
         });
+
         await test.step('Y el usuario busca al empleado por su ID', async () => {
             await employeePage.clickOnPIMModule();
             await employeePage.fillEmployeeInfoIDInput(uniqueID);
             await employeePage.searchClick();
         });
+
         await test.step('Entonces el empleado se encuentra en la lista de empleados', async () => {
             await employeePage.page.waitForLoadState('networkidle');
             await employeePage.page.waitForTimeout(2000); // Agregar un retraso manual
             const locator = employeePage.idColumnValues(uniqueID);
             await expect.soft(locator).toBeVisible({ timeout: 10000 });
-});
+        });
+
         await test.step('Entonces el usuario navega a la página de empleados nuevamente', async () => {
             await employeePage.clickOnPIMModule();
         });
+
         await test.step('Y el usuario se dirige hacia la sección de agregar empleados nuevamente', async () => {
             await employeePage.goToAddEmployeePage();
         });
+
         await test.step('Y el usuario llena los campos requeridos con el mismo ID', async () => {
             await employeePage.fillAddEmployee("Adam", 'Smith', uniqueID);
         });
+
         await test.step('Y el usuario intenta guardar al empleado', async () => {
             await employeePage.saveNewEmployeeClick();
         });
+
         await test.step('Y se muestra un mensaje de error', async () => {
             await employeePage.page.waitForLoadState('networkidle');
             await employeePage.page.waitForSelector("//span[text()='Employee Id already exists']");
@@ -126,32 +188,40 @@ test.describe('[US02] Gestión de empleados | Agregar empleados incorrectamente'
         });
     });
 
-    test('Validar error al ingresar un empleado con numeros o simbolos para el sector nombre y apellido, y letras para el sector ID.', async ({ employeePage }) => {
+    test('Escenario 6 | Validar error al ingresar un empleado con numeros o simbolos para el sector nombre y apellido, y letras para el sector ID.', async ({ employeePage }) => {
         const stringID = 'IDNumber';
         await test.step('Dado que el usuario se encuentra en la página de inicio de sesión', async () => { });
+
         await test.step('Cuando el usuario navega a la página de empleados', async () => {
             await employeePage.clickOnPIMModule();
         });
+
         await test.step('Y se dirige hacia la sección de agregar empleados', async () => {
             await employeePage.goToAddEmployeePage();
         });
+
         await test.step('Y el usuario no llena ningun campo requerido', async () => {
             await employeePage.fillAddEmployee('124%6/', '124%6/', stringID);
         });
+
         await test.step('Y el usuario guarda al empleado', async () => {
             await employeePage.saveNewEmployeeClick();
         });
+
         await test.step('Entonces se muestra un mensaje de error encima del InputFirstName', async () => {
             await employeePage.page.waitForSelector("//div[@class='oxd-input-group oxd-input-field-bottom-space']//span[1]");
             await expect.soft(employeePage.page.locator("//div[@class='oxd-input-group oxd-input-field-bottom-space']//span[1]")).toHaveText('Only letters are allowed');
         });
+
         await test.step('Y se muestra un mensaje de error encima del InputLastName', async () => {
             await employeePage.page.waitForSelector("//div[@class='oxd-input-group oxd-input-field-bottom-space']//span[2]");
             await expect.soft(employeePage.page.locator("//div[@class='oxd-input-group oxd-input-field-bottom-space']//span[2]")).toHaveText('Only letters are allowed');
         });
+
         await test.step('Y se muestra un mensaje de error encima del InputID', async () => {
-        await expect.soft(employeePage.page.getByText('Only numbers allowed')).toBeVisible({ timeout: 10000 });
+            await expect.soft(employeePage.page.getByText('Only numbers allowed')).toBeVisible({ timeout: 10000 });
         });
+
         await test.step('Entonces no se podra agregar el empleado', async () => {
             await expect.soft(employeePage.idColumnValues(stringID)).toBeHidden();
         });
--- tests/e2e/login/login-success.spec.ts
+++ tests/e2e/login/login-success.spec.ts
@@ -1,14 +1,89 @@
 import { expect, test } from '../../fixtures';
 
+const user = process.env.USER;
+const password = process.env.PASSWORD;
 
-test.describe('[US01] Inicio de sesión | Iniciar sesión con mis credenciales', () => {
+test.describe('[US01] Inicio de sesión', () => {
     
     test('Validar inicio de sesión exitoso', async ({ loginPage }) => {
-        await loginPage.goToLoginPage();
-        await loginPage.login();
-        await expect(loginPage.page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
-        await expect(loginPage.page.locator('ul.oxd-main-menu')).toBeVisible();
+        await test.step('Dado que el usuario navega a la página de inicio de sesión', async () => {
+            await loginPage.goToLoginPage();
+        });
+
+        await test.step('Cuando el usuario ingresa credenciales válidas y hace clic en "Login"', async () => {
+            await loginPage.login();
+        });
+
+        await test.step('Entonces el usuario debería ser redirigido al dashboard y ver el menú principal', async () => {
+            await expect(loginPage.page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+            await expect(loginPage.page.locator('ul.oxd-main-menu')).toBeVisible();
+        });
+    });
+
+    test('Validar error al iniciar sesión con username incorrecto', async ({ loginPage }) => {
+        await test.step('Dado que el usuario navega a la página de inicio de sesión', async () => {
+            await loginPage.goToLoginPage();
+        });
+
+        await test.step('Cuando el usuario ingresa un nombre de usuario incorrecto y la contraseña correcta', async () => {
+            await loginPage.login('incorrecto', password);
+        });
+
+        await test.step('Entonces debería aparecer un mensaje de error indicando "Invalid credentials"', async () => {
+            await expect(loginPage.page.locator('div.oxd-alert-content.oxd-alert-content--error')).toHaveText('Invalid credentials');
+        });
+    });
+
+    test('Validar error al iniciar sesión con password incorrecto', async ({ loginPage }) => {
+        await test.step('Dado que el usuario navega a la página de inicio de sesión', async () => {
+            await loginPage.goToLoginPage();
+        });
+
+        await test.step('Cuando el usuario ingresa el nombre de usuario correcto y una contraseña incorrecta', async () => {
+            await loginPage.login(user, 'incorrecta');
+        });
+
+        await test.step('Entonces debería aparecer un mensaje de error indicando "Invalid credentials"', async () => {
+            await expect(loginPage.page.locator('div.oxd-alert-content.oxd-alert-content--error')).toHaveText('Invalid credentials');
+        });
+    });
+
+    test('Validar inicio de sesión sin ingresar username', async ({ loginPage }) => {
+        await test.step('Dado que el usuario navega a la página de inicio de sesión', async () => {
+            await loginPage.goToLoginPage();
+        });
+
+        await test.step('Cuando el usuario deja el campo del nombre de usuario vacío y ingresa la contraseña correcta', async () => {
+            await loginPage.login('', password);
+        });
+
+        await test.step('Entonces debería aparecer un mensaje de error indicando "Required"', async () => {
+            await expect(loginPage.page.locator('span.oxd-text.oxd-text--span')).toHaveText('Required');
+        });
+    });
+
+    test('Validar inicio de sesión sin ingresar contraseña', async ({ loginPage }) => {
+        await test.step('Dado que el usuario navega a la página de inicio de sesión', async () => {
+            await loginPage.goToLoginPage();
+        });
+
+        await test.step('Cuando el usuario ingresa el nombre de usuario correcto y deja el campo de la contraseña vacío', async () => {
+            await loginPage.login(user, '');
+        });
+
+        await test.step('Entonces debería aparecer un mensaje de error indicando "Required"', async () => {
+            await expect(loginPage.page.locator('span.oxd-text.oxd-text--span')).toHaveText('Required');
+        });
+    });
+
+    test('Validar boton login deshabilitado sin credenciales', async ({ loginPage }) => {
+        await test.step('Dado que el usuario navega a la página de inicio de sesión', async () => {
+            await loginPage.goToLoginPage();
+        });
+
+        await test.step('Entonces el botón "Login" debería estar deshabilitado cuando los campos están vacíos', async () => {
+            await expect(loginPage.page.locator("button[type='submit']")).toBeDisabled();
+        });
     });
 
 });
-
